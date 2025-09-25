46612642c8c6288243034d6b73370e1a47f047ce
M Umer <mumer@Ms-MacBook-Pro.local>
2025-09-25 14:55:02 +0500

    bulk/get_create

--- blogApp/blog/serializers.py
+++ blogApp/blog/serializers.py
@@ -5,3 +5,9 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = '__all__'
+
+    def validate_Post_title(self,data):
+       if "@" in data:
+        raise serializers.ValidationError("Title cannot contain @ symbol")
+         
+       return data
--- blogApp/blog/views.py
+++ blogApp/blog/views.py
@@ -24,27 +24,83 @@ class Blog(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self,request):
-        blog=Post.objects.all()
-        serializer=PostSerializer(blog,many=True)
-        # user=request.user
-        # print("user " ,user)
-        # print("rrrr ",serializer)
-        return Response(serializer.data)
+            blog=Post.objects.all()
+            serializer=PostSerializer(blog,many=True)
+            # user=request.user
+            # print("user " ,user)
+            # print("rrrr ",serializer)
+            return Response(serializer.data)
     
+    
+   
     def post(self,request):
-        # print("hello",request.data)
+
+        #----bulk create----
+
+        blogs=request.data
+        bulk_create=[Post(**blog) for blog in blogs ]
+        Post.objects.bulk_create(bulk_create)
+        return Response(status=status.HTTP_201_CREATED)
+    
+     #----------create-------
+             
+        print("hello",request.data)
         title = request.data.get("Post_title")
         content = request.data.get("Post_content")
         Post.objects.create(Post_title=title,Post_content=content)
-        return Response(status=status.HTTP_201_CREATED)
-        # serializer = PostSerializer(data=request.data)
-        # if serializer.is_valid():
-        #     serializer.save()
-        #     return Response(serializer.data)
-        # return Response(serializer.errors)
+
+
+
+        #-----get_or_create-------
+        title = request.data.get("Post_title")
+        content = request.data.get("Post_content")
+
+        post, created = Post.objects.get_or_create(
+        Post_title=title,
+        defaults={"Post_content": content}
+        )
+
+        serializer = PostSerializer(post)
+
+        return Response(serializer.data)
+
+       #-----serializer-------
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors)
+
+       
     
+
+    #bulk update
     def put(self,request,id):
         print("hello",request.data)
+
+        blogs=Post.objects.all()[:2]
+        for blog in blogs:
+            blog.Post_content="automate"
+            blog.Post_title="n88n"
+        Post.objects.bulk_update(blogs,['Post_content','Post_title'])
+
+
+    #-----------update or create--------
+
+        title = request.data.get("Post_title")
+        content = request.data.get("Post_content")
+
+        post, created = Post.objects.update_or_create(
+            Post_title=title,   
+            defaults={"Post_content": content}  
+        )
+
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+
+        #----simple update---------.
+        blog=request.data
         title = request.data.get("Post_title")
         content = request.data.get("Post_content")
         blog=Post.objects.get(pk=id)
@@ -52,8 +108,12 @@ class Blog(APIView):
         blog.Post_content=request.data.get("Post_content")
         blog.save()
         return Response(status=status.HTTP_200_OK)
-
     
+
+
+
+
+    #------delete-----------
     def delete(self,request,id):
         blog=Post.objects.get(pk=id)
         blog.delete()
@@ -65,73 +125,3 @@ class Blog(APIView):
     
 
 
-#     model=Post
-#     fields=['Post_title', 'Post_content']
-#     template_name='blog/create_post.html'
-#     success_url = reverse_lazy('show') 
-
-
-# class show_all(ListView):
-#     model=Post
-#     template_name='blog/showall.html'
-
-
-# class update_blog(UpdateView):
-#     model=Post
-#     fields=['Post_title', 'Post_content']   
-#     template_name = 'blog/update.html'
-#     success_url = reverse_lazy('show')
-
-# class delete_blog(DeleteView):
-#     model=Post
-#     success_url=reverse_lazy('show')
-#     template_name = "blog/confirm_delete.html"
-
-#     # if request.method=='POST':
-    #     title=request.POST.get("title")
-    #     # print("umerrr",title)
-    #     content=request.POST.get("Content")
-    #     author_name=request.POST.get("Author")
-    #     author=Author.objects.get(Author_name=author_name)
-    #     print("testing ",title)
-    #     post=Post(Post_title=title,
-    #               Post_content=content,
-    #               A_name=author)
-    #     post.save()
-    #     print("post created",post)
-    # return render(request,"blog/create_post.html")
-
-
-# def show_all(request):
-#     all_posts=Post.objects.all()
-#     print("umerrr",all_posts)
-#     return render(request,"blog/showall.html",{"all_posts":all_posts})
-
-# def update_blog(request,dubai):
-#     print("id",dubai)
-#     blog_by_id=Post.objects.get(id=dubai)
-#     # print(blog_by_id)
-#     if request.method=='POST':
-#         title=request.POST.get("title")
-#         print("new_title",title)
-#         content=request.POST.get("Content")
-        
-#         blog_by_id.Post_title=title
-#         blog_by_id.Post_content=content
-#         blog_by_id.save()
-       
-
-#         print( blog_by_id.Post_title,"after updating")
-#         return redirect("show_all")
-        
-
-
-#     return render(request,"blog/update.html",{"blog_by_id":blog_by_id})
-
-
-# def delete_blog(request,id):
-#     blog=Post.objects.get(id=id)
-#     print("deleting blog ",blog.Post_title)
-#     blog.delete()
-#     return redirect("show_all")
-    
